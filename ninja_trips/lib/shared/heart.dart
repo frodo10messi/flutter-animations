2f129e0dcec9f1ed9fb8bfcb7b337e31ec3176e6
abdul ahad <abdul_ahad1996@live.com>
2020-11-20 03:33:11 +0500

    Update heart.dart
    added explanation

--- ninja_trips/lib/shared/heart.dart
+++ ninja_trips/lib/shared/heart.dart
@@ -13,19 +13,30 @@ class _HeartState extends State<Heart> with SingleTickerProviderStateMixin {
   @override
   void initState() {
     super.initState();
+    //EXPLICIT ANIMATION(WE CONTROL THE ANIMATION)
+    //IMPLICIT ANIMATION(WE DON'T CONTROL ANIMATION,BUILT IN )
 
+    //animation controller gives us fine graint control over animation
+    //this runs a default tween from 0 to 1 in 2 ms.
+    //vsync part actually is like a clock and makes our widget a ticker when its on screen . we need to use a mixin for this
     _controller = AnimationController(
       duration: Duration(milliseconds: 200),
       vsync: this,
     );
 
+    //a tween is basically which has a beg and end point . from 0 - 1 and it interpolates from 0 to 1
+    //by default we only get a default tween from 0 to 1 which cannot be used for colors so we use a color tween and use animate methord so we actually get a controller 
     _colorAnimation = ColorTween(begin: Colors.grey[400], end: Colors.red).animate(_controller);
 
+    //every time a controllers value changes we get thus call back
+    //controller.forward makes the value go from 0 to 1 
+    //controller.dismiss makes the value from 1 to 0
     _controller.addListener(() {
       // print(_controller.value);
       // print(_colorAnimation.value);
     });
 
+    //we also get a animation status call back stating if its completed or reversed 
     _controller.addStatusListener((status) {
       if (status == AnimationStatus.completed) {
         setState(() {
@@ -47,6 +58,7 @@ class _HeartState extends State<Heart> with SingleTickerProviderStateMixin {
     _controller.dispose();
   }
 
+  //we can use setstate to listen for animation changes in values or animated builder 
   @override
   Widget build(BuildContext context) {
     return AnimatedBuilder(
